2861b2ba68f76b54c4dac41797f5f35eba027c45
zhouyehua <zhouyehua2014@163.com>
2020-06-10 15:55:45 +0800

    utils包的更新

--- main.go
+++ main.go
@@ -1,38 +1,51 @@
 package main
 
 import (
+	"time"
+
+	"CmsProject/config"
+	"CmsProject/controller"
+	"CmsProject/datasource"
+	"CmsProject/service"
+
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/context"
+	"github.com/kataras/iris/mvc"
 	"github.com/kataras/iris/sessions"
-	"time"
 )
-
-/*
- *程序主入口
- */
 
 func main() {
 	app := newApp()
 
-	//应用app设置
-	configuration(app)
+	//应用App设置
+	configation(app)
+
 	//路由设置
-	//mvcHandle(app)
-	//config := config.InitConfig()
-	app.Run(iris.Addr(":9000"))
+	mvcHandle(app)
+
+	config := config.InitConfig()
+	addr := ":" + config.Port
+	app.Run(
+		iris.Addr(addr), //在端口8080进行监听
+		iris.WithoutServerError(iris.ErrServerClosed), //无服务错误提示
+		iris.WithOptimizations,                        //对json数据序列化更快的配置
+	)
 }
 
-//构建APP
+//构建App
 func newApp() *iris.Application {
 	app := iris.New()
-	//设置日志级别
+
+	//设置日志级别  开发阶段为debug
 	app.Logger().SetLevel("debug")
+
 	//注册静态资源
 	app.HandleDir("/static", "./static")
 	app.HandleDir("/manage/static", "./static")
 	app.HandleDir("/img", "./static/img")
+
 	//注册视图文件
-	app.RegisterView(iris.HTML("./static", "html"))
+	app.RegisterView(iris.HTML("./static", ".html"))
 	app.Get("/", func(context context.Context) {
 		context.View("index.html")
 	})
@@ -40,46 +53,74 @@ func newApp() *iris.Application {
 	return app
 }
 
-//项目设置
-func configuration(app *iris.Application) {
-	//配置字符编码
+/**
+ * MVC 架构模式处理
+ */
+func mvcHandle(app *iris.Application) {
+
+	//启用session
+	sessManager := sessions.New(sessions.Config{
+		Cookie:  "sessioncookie",
+		Expires: 24 * time.Hour,
+	})
+
+	engine := datasource.NewMysqlEngine()
+
+	//管理员模块功能
+	adminService := service.NewAdminService(engine)
+
+	admin := mvc.New(app.Party("/admin"))
+	admin.Register(
+		adminService,
+		sessManager.Start,
+	)
+	admin.Handle(new(controller.AdminController))
+
+	//用户功能模块
+	userService := service.NewUserService(engine)
+
+	user := mvc.New(app.Party("/v1/users"))
+	user.Register(
+		userService,
+		sessManager.Start,
+	)
+	user.Handle(new(controller.UserController))
+
+	//统计功能模块
+	statisService := service.NewStatisService(engine)
+	statis := mvc.New(app.Party("/statis/{model}/{date}/"))
+	statis.Register(
+		statisService,
+		sessManager.Start,
+	)
+	statis.Handle(new(controller.StatisController))
+}
+
+/**
+ * 项目设置
+ */
+func configation(app *iris.Application) {
+
+	//配置 字符编码
 	app.Configure(iris.WithConfiguration(iris.Configuration{
 		Charset: "UTF-8",
 	}))
+
 	//错误配置
 	//未发现错误
 	app.OnErrorCode(iris.StatusNotFound, func(context context.Context) {
 		context.JSON(iris.Map{
-			"err_msg": iris.StatusNotFound,
-			"msg":     "not found",
-			"date":    iris.Map{},
+			"errmsg": iris.StatusNotFound,
+			"msg":    " not found ",
+			"data":   iris.Map{},
 		})
 	})
+
 	app.OnErrorCode(iris.StatusInternalServerError, func(context context.Context) {
 		context.JSON(iris.Map{
-			"err_msg": iris.StatusInternalServerError,
-			"msg":     "internal error",
-			"date":    iris.Map{},
+			"errmsg": iris.StatusInternalServerError,
+			"msg":    " internal error ",
+			"data":   iris.Map{},
 		})
 	})
-}
-
-//MVC架构模式处理
-func mvcHandle(app *iris.Application) {
-	//启用session
-	sessionManager := sessions.New(sessions.Config{
-		Cookie:                      "sessioncookie",
-		CookieSecureTLS:             false,
-		AllowReclaim:                false,
-		Encode:                      nil,
-		Decode:                      nil,
-		Encoding:                    nil,
-		Expires:                     24 * time.Hour,
-		SessionIDGenerator:          nil,
-		DisableSubdomainPersistence: false,
-	})
-	//engine := datasource.NewMySQLEngine()
-
-	//管理员模块功能
-	//adminService :=
 }
